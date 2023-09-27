fdb0979771e36f6c4ec104fbef9acfe7159c3364
Steven Charles P. Palabyab <charles.halcyondigital@gmail.com>
2023-09-27 15:42:44 +0800

    refactor: folder naming
    Signed-off-by: Steven Charles P. Palabyab <charles.halcyondigital@gmail.com>

--- src/main/app/interfaces/data/data.open-data.interface.ts
+++
@@ -1,3 +0,0 @@
-type IDataName = 'permission' | 'role' | 'user' | 'peer' | 'auth';
-
-export default IDataName;
---
+++ src/types/peer-data.d.ts
@@ -0,0 +1,42 @@
+import IResponse from 'App/interfaces/pos/pos.response.interface';
+
+declare global {
+  type POSChannels =
+    | 'auth:me'
+    | 'auth:sign-in'
+    | 'auth:sign-up'
+    | 'auth:sign-out'
+    | 'peer:request'
+    | 'peer:sync'
+    | 'permission:archive'
+    | 'permission:show'
+    | 'role:show'
+    | 'role:create'
+    | 'role:update'
+    | 'role:delete'
+    | 'role:archive'
+    | 'user:show'
+    | 'user:create'
+    | 'user:update'
+    | 'user:archive';
+
+  interface PeerDataContract {
+    systemKey: string | null;
+    type: 'request' | 'response';
+    token: string;
+    request?:
+      | {
+          name: POSChannels;
+          body?: any;
+        }
+      | undefined;
+    response?:
+      | {
+          name: POSChannels;
+          body?: IResponse;
+        }
+      | undefined;
+  }
+}
+
+export {};
