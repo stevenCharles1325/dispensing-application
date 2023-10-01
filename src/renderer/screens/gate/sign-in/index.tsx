19d6a009f889717332c7187beb90feb679326ef6
Steven Charles P. Palabyab <charles.halcyondigital@gmail.com>
2023-10-01 20:40:56 +0800

    chore: user cookie on sign-in success
    Signed-off-by: Steven Charles P. Palabyab <charles.halcyondigital@gmail.com>

--- src/renderer/screens/gate/sign-in/index.tsx
+++ src/renderer/screens/gate/sign-in/index.tsx
@@ -3,11 +3,17 @@ import AppLogo from 'UI/components/Logo/AppLogo';
 import Input from 'UI/components/TextField/Input';
 import { Button, Link } from '@mui/material';
 import useAlert from 'UI/hooks/useAlert';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import IPOSError from 'App/interfaces/pos/pos.error.interface';
+import { useNavigate } from 'react-router-dom';
+import useUser from 'UI/stores/user';
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const user = useUser((store) => store);
+  const { setUser } = user;
   const { displayAlert } = useAlert();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,11 +23,26 @@ export default function SignIn() {
       password,
     });
 
-    const type = res.errors?.length ? 'error' : 'success';
-    const message = (res.errors?.[0] as IPOSError)?.message ?? 'Login Success';
+    if (res.status === 'ERROR') {
+      const type = 'error';
+      const message = (res.errors as unknown as IPOSError[])[0]?.message;
 
-    displayAlert?.(message as string, type);
+      displayAlert?.(message as string, type);
+    } else {
+      const data = res.data as unknown as any;
+
+      setUser('token', data.token);
+      setUser('refresh_token', data.refresh_token);
+      setUser(data.user);
+
+      displayAlert?.('Login success', 'success');
+      console.log(user);
+    }
   };
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
 
   return (
     <div className="w-screen h-screen bg-transparent flex justify-center items-center shadow-inner">
