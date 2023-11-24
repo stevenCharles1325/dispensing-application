edaa6b009efd456e2ca27c7452e5cbc84ad9a01f
stevenCharles1325 <stevencharles1325@gmail.com>
2023-11-24 11:03:07 +0800

    fix: allow edit on non-owner users
    Signed-off-by: stevenCharles1325 <stevencharles1325@gmail.com>

--- src/main/app/events/auth/auth.update-me.event.ts
+++ src/main/app/events/auth/auth.update-me.event.ts
@@ -34,99 +34,78 @@ export default class AuthUpdateMeEvent implements IEvent {
       const { isChangingPassword = false } = userUpdate;
       const authService = Provider.ioc<IAuthService>('AuthProvider');
 
-      const requesterHasPermission =
-        eventData.user.hasPermission?.('update-user');
+      const _user = await UserRepository.findOneByOrFail({
+        id,
+      });
 
-      if (requesterHasPermission) {
-        const _user = await UserRepository.findOneByOrFail({
-          id,
-        });
+      if (isChangingPassword) {
+        if (userUpdate.password?.length) {
+          if (
+            bcrypt.compareSync(userUpdate.current_password, _user.password)
+          ) {
+            const saltRound = 10;
+            const salt = bcrypt.genSaltSync(saltRound);
+            userUpdate.password = bcrypt.hashSync(userUpdate.password, salt);
+          } else {
+            await Bull('AUDIT_JOB', {
+              user_id: user.id as number,
+              resource_id: id.toString(),
+              resource_table: 'users',
+              resource_id_type: 'integer',
+              action: 'update',
+              status: 'FAILED',
+              description: `User ${user.fullName} has failed to update their profile due to incorrect password`,
+            });
 
-        if (isChangingPassword) {
-          if (userUpdate.password?.length) {
-            if (
-              bcrypt.compareSync(userUpdate.current_password, _user.password)
-            ) {
-              const saltRound = 10;
-              const salt = bcrypt.genSaltSync(saltRound);
-              userUpdate.password = bcrypt.hashSync(userUpdate.password, salt);
-            } else {
-              await Bull('AUDIT_JOB', {
-                user_id: user.id as number,
-                resource_id: id.toString(),
-                resource_table: 'users',
-                resource_id_type: 'integer',
-                action: 'update',
-                status: 'FAILED',
-                description: `User ${user.fullName} has failed to update their profile due to incorrect password`,
-              });
-
-              return {
-                errors: [
-                  { field: 'current_password', message: 'Incorrect password' },
-                ],
-                status: 'ERROR',
-              } as unknown as IResponse<string[]>;
-            }
+            return {
+              errors: [
+                { field: 'current_password', message: 'Incorrect password' },
+              ],
+              status: 'ERROR',
+            } as unknown as IResponse<string[]>;
           }
         }
-
-        const updatedUser = UserRepository.merge(_user, userUpdate);
-        const errors = await validator(updatedUser);
-
-        if (errors.length) {
-          return {
-            errors,
-            code: 'VALIDATION_ERR',
-            status: 'ERROR',
-          } as unknown as IResponse<IPOSValidationError[]>;
-        }
-
-        const data: User = await UserRepository.save(updatedUser);
-
-        const user_data = {
-          id: data.id,
-          email: data.email,
-          image_url: data.image_url,
-          first_name: data.first_name,
-          last_name: data.last_name,
-          full_name: data.fullName(),
-          phone_number: data.phone_number,
-          notification_status: data.notification_status,
-          role: data.role,
-        };
-
-        const [token, refresh_token] = authService.generateToken(user_data);
-        const payload = {
-          token,
-          refresh_token,
-          user: data.serialize('password') as UserDTO,
-          token_expires_at: parseTimeExpression(
-            authService.config.token_expires_at
-          ),
-          refresh_token_expires_at: parseTimeExpression(
-            authService.config.refresh_token_expires_at
-          ),
-        };
-
-        authService.setAuthUser(payload);
-
-        await Bull('AUDIT_JOB', {
-          user_id: user.id as number,
-          resource_id: id.toString(),
-          resource_table: 'users',
-          resource_id_type: 'integer',
-          action: 'update',
-          status: 'SUCCEEDED',
-          description: `User ${user.fullName} has successfully updated their profile`,
-        });
-
-        return {
-          data,
-          code: 'REQ_OK',
-          status: 'SUCCESS',
-        } as IResponse<typeof data>;
       }
+
+      const updatedUser = UserRepository.merge(_user, userUpdate);
+      const errors = await validator(updatedUser);
+
+      if (errors.length) {
+        return {
+          errors,
+          code: 'VALIDATION_ERR',
+          status: 'ERROR',
+        } as unknown as IResponse<IPOSValidationError[]>;
+      }
+
+      const data: User = await UserRepository.save(updatedUser);
+
+      const user_data = {
+        id: data.id,
+        email: data.email,
+        image_url: data.image_url,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        full_name: data.fullName(),
+        phone_number: data.phone_number,
+        notification_status: data.notification_status,
+        role: data.role,
+      };
+
+      const [token, refresh_token] = authService.generateToken(user_data);
+      const payload = {
+        token,
+        refresh_token,
+        user: data.serialize('password') as UserDTO,
+        token_expires_at: parseTimeExpression(
+          authService.config.token_expires_at
+        ),
+        refresh_token_expires_at: parseTimeExpression(
+          authService.config.refresh_token_expires_at
+        ),
+      };
+
+      authService.setAuthUser(payload);
 
       await Bull('AUDIT_JOB', {
         user_id: user.id as number,
@@ -134,14 +113,15 @@ export default class AuthUpdateMeEvent implements IEvent {
         resource_table: 'users',
         resource_id_type: 'integer',
         action: 'update',
-        status: 'FAILED',
-        description: `User ${user.fullName} has failed to update their profile`,
+        status: 'SUCCEEDED',
+        description: `User ${user.fullName} has successfully updated their profile`,
       });
 
       return {
-        errors: ['You are not allowed to update their profile'],
-        status: 'ERROR',
-      } as unknown as IResponse<string[]>;
+        data,
+        code: 'REQ_OK',
+        status: 'SUCCESS',
+      } as IResponse<typeof data>;
     } catch (err) {
       const error = handleError(err);
       console.log('ERROR HANDLER OUTPUT: ', error);
